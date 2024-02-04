0494f205cbceb1e6dbb0fd2927834b436d6a5f71
Sergii Kurdybakha <s.kurdybakha@gmail.com>
2024-02-04 19:36:52 +0200

    Refactoring components from class to hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -27,7 +27,9 @@ const App = () => {
 
         const { data } = await searchImages(search, page);
 
-        setImages(data.hits?.length ? [...images, ...data.hits] : images);
+        setImages(prevImages =>
+          data.hits?.length ? [...prevImages, ...data.hits] : prevImages
+        );
 
         if (data.totalHits > IMAGES_PER_PAGE) {
           displayLoadMoreButton(true);
@@ -90,111 +92,4 @@ const App = () => {
   );
 };
 
-// export class App extends PureComponent {
-//   state = {
-//     search: '',
-//     images: [],
-//     loading: false,
-//     isLoadMore: false,
-//     error: null,
-//     page: 1,
-//     modalOpen: false,
-//     largeImage: {},
-//   };
-
-//   async componentDidUpdate(prevProps, prevState) {
-//     const { search, page } = this.state;
-//     if (search && (search !== prevState.search || page !== prevState.page)) {
-//       this.fetchPosts();
-//     }
-//   }
-
-//   async fetchPosts() {
-//     const { search, page } = this.state;
-//     try {
-//       this.setState({
-//         loading: true,
-//       });
-//       const { data } = await searchImages(search, page);
-
-//       this.setState(({ images }) => ({
-//         images: data.hits?.length ? [...images, ...data.hits] : images,
-//       }));
-
-//       if (data.totalHits > IMAGES_PER_PAGE) {
-//         this.displayLoadMoreButton(true);
-//       }
-
-//       if (IMAGES_PER_PAGE * page >= data.totalHits) {
-//         this.displayLoadMoreButton(false);
-//       }
-//     } catch (error) {
-//       this.setState({
-//         error: error.message,
-//       });
-//     } finally {
-//       this.setState({
-//         loading: false,
-//       });
-//     }
-//   }
-
-//   handleSearch = ({ search }) => {
-//     this.setState({
-//       search,
-//       images: [],
-//       page: 1,
-//     });
-//   };
-
-//   loadMore = () => {
-//     this.setState(({ page }) => ({ page: page + 1 }));
-//   };
-
-//   displayLoadMoreButton = isShow => {
-//     this.setState({ isLoadMore: isShow });
-//   };
-
-//   showModal = largeImageURL => {
-//     this.setState({
-//       modalOpen: true,
-//       largeImage: { largeImageURL },
-//     });
-//   };
-
-//   closeModal = () => {
-//     this.setState({
-//       modalOpen: false,
-//       largeImage: {},
-//     });
-//   };
-
-//   render() {
-//     const { handleSearch, loadMore, showModal, closeModal } = this;
-//     const { images, loading, isLoadMore, error, modalOpen, largeImage } =
-//       this.state;
-//     // const imagesLength = Boolean(images.length);
-//     return (
-//       <div>
-//         <Searchbar onSubmit={handleSearch} />
-//         {error && <p>{error}</p>}
-//         {loading && <Loader />}
-
-//         <ImageGallery showModal={showModal} items={images} />
-//         {isLoadMore && (
-//           <div className={css.loadMoreWrapper}>
-//             <Button onClick={loadMore} type="button">
-//               Load More
-//             </Button>
-//           </div>
-//         )}
-//         {modalOpen && (
-//           <Modal close={closeModal}>
-//             <img src={largeImage.largeImageURL} alt="" />
-//           </Modal>
-//         )}
-//       </div>
-//     );
-//   }
-// }
 export default App;
